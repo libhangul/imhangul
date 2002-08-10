f2e803276ec318ed6cafa3ab80534062b6ed8fe0
Choe Hwanjin <choe.hwanjin@gmail.com>
2002-08-10 21:22:26 +0900

    test code
    git-svn-id: http://kldp.net/svn/imhangul/trunk@34 e8eb2e9a-03fc-0310-a997-8289724496f5

--- imhangul/test.sh
+++ imhangul/test.sh
@@ -5,7 +5,7 @@ if [ ! -f $immodules_file ]; then
 	gtk-query-immodules-2.0 .libs/*.so > $immodules_file
 fi
 
-export GTK_RC_FILES=./gtkrc
+export GTK_RC_FILES=$HOME/.gtkrc-2.0:./gtkrc
 export GTK_IM_MODULE_PATH=.libs/
 export GTK_IM_MODULE_FILE=./$immodules_file
 exec ./entry -display :1
