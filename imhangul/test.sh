43cf5e6a9ae95937d81e8938da3f805766637552
Choe Hwanjin <choe.hwanjin@gmail.com>
2002-08-14 15:17:39 +0900

    gtk2.0.6에 맞는 환경변수 사용
    git-svn-id: http://kldp.net/svn/imhangul/trunk@38 e8eb2e9a-03fc-0310-a997-8289724496f5

--- imhangul/test.sh
+++ imhangul/test.sh
@@ -5,6 +5,7 @@ if [ ! -f $immodules_file ]; then
 	gtk-query-immodules-2.0 .libs/*.so > $immodules_file
 fi
 
+export GTK_RC_FILES=`pwd`/gtkrc:$HOME/.gtkrc-2.0
 export GTK2_RC_FILES=`pwd`/gtkrc:$HOME/.gtkrc-2.0
 export GTK_IM_MODULE_PATH=.libs/
 export GTK_IM_MODULE_FILE=$immodules_file
