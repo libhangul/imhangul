f2e99a9d03c4fe68bfaa896418a39c20da673fcc
Choe Hwanjin <choe.hwanjin@gmail.com>
2002-10-25 23:40:11 +0900

    manual mode 단축키 Ctrl-Hangul 추가
    git-svn-id: http://kldp.net/svn/imhangul/trunk/imhangul@80 e8eb2e9a-03fc-0310-a997-8289724496f5

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 immodules_file=./immodules
 gtk-query-immodules-2.0 .libs/*.so > $immodules_file
 
-export GTK_IM_MODULE=hangul3f
+export GTK_IM_MODULE=hangul32
 export GTK_RC_FILES=`pwd`/gtkrc
 export GTK2_RC_FILES=`pwd`/gtkrc
 export GTK_IM_MODULE_PATH=.libs/
