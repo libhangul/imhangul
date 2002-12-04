b9af9628faa2eab2d9d3a5a4baef66b8434f3d7c
Choe Hwanjin <choe.hwanjin@gmail.com>
2002-12-04 16:29:07 +0900

    im_hangul2_jongseong_dicompose 테이블 수정 (ㅆ -> ㅅ + ㅅ)
    git-svn-id: http://kldp.net/svn/imhangul/trunk/imhangul@112 e8eb2e9a-03fc-0310-a997-8289724496f5

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ export GTK_RC_FILES=`pwd`/gtkrc
 export GTK2_RC_FILES=`pwd`/gtkrc
 export GTK_IM_MODULE_PATH=.libs/
 export GTK_IM_MODULE_FILE=$immodules_file
-#export GDK_USE_XFT=1
+export GDK_USE_XFT=1
 exec ./entry
 # gtk-demo
 # exec ./xchat
-# exec gedit
+exec gedit
