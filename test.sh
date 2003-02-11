9b2eb3c170b5a4a371268e5fb7f4c8ee7d345a84
Choe Hwanjin <choe.hwanjin@gmail.com>
2003-02-11 15:58:28 +0900

    im_hangul_set_client_window 의 인자를 제대로 처리함
    git-svn-id: http://kldp.net/svn/imhangul/trunk/imhangul@163 e8eb2e9a-03fc-0310-a997-8289724496f5

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ export GTK_IM_MODULE=hangul2
 export GTK_IM_MODULE_PATH=.libs/
 export GTK_IM_MODULE_FILE=$immodules_file
 #export GDK_USE_XFT=1
-exec ./entry
+# exec ./entry
 # exec gtk-demo
 # exec ./xchat
-#exec /usr/local/bin/gedit
+exec /usr/local/bin/gedit
 #exec /usr/local/bin/pan
