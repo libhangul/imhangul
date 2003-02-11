7346374498baaa0ffca1c21bd587ad220a39683c
Choe Hwanjin <choe.hwanjin@gmail.com>
2003-02-11 15:48:16 +0900

    im_hangul_set_client_window 에서 인자 값 체크
    git-svn-id: http://kldp.net/svn/imhangul/trunk@162 e8eb2e9a-03fc-0310-a997-8289724496f5

--- imhangul/test.sh
+++ imhangul/test.sh
@@ -4,12 +4,13 @@ immodules_file=./gtk.immodules
 gtk-query-immodules-2.0 .libs/*.so > $immodules_file
 
 export GTK_IM_MODULE=hangul2
-export GTK_RC_FILES=`pwd`/gtkrc
-export GTK2_RC_FILES=`pwd`/gtkrc
+#export GTK_RC_FILES=`pwd`/gtkrc
+#export GTK2_RC_FILES=`pwd`/gtkrc
 export GTK_IM_MODULE_PATH=.libs/
 export GTK_IM_MODULE_FILE=$immodules_file
-export GDK_USE_XFT=1
+#export GDK_USE_XFT=1
 exec ./entry
-#exec gtk-demo
+# exec gtk-demo
 # exec ./xchat
-exec gedit
+#exec /usr/local/bin/gedit
+#exec /usr/local/bin/pan
