c7b14ae173eadc9aa1229de6889f1c39b8c02397
Choe Hwanjin <choe.hwanjin@gmail.com>
2011-01-02 17:15:03 +0900

    libhangul의 hangul_ic_get_keyboard_name() 인터페이스를 사용하도록 바꿈
    libhangul에서 지원하는 키보드가 하나씩 늘때마다 imhangul의 메뉴 엔트리를
    하나씩 늘려서 릴리스 하려면 좀 불편하다. 그래서 libhangul이 지원하는 
    키보드 리스트를 가져오는 함수를 만들고 그 함수 인터페이스를 이용해서
    imhangul에서 키보드 리스트를 보여주도록 구현한다.
    
    
    git-svn-id: http://kldp.net/svn/imhangul/imhangul/trunk@390 e8eb2e9a-03fc-0310-a997-8289724496f5

--- imhangul.c
+++ imhangul.c
@@ -30,82 +30,35 @@
 #include "gettext.h"
 #include "gtkimcontexthangul.h"
 
+/* 키보드 이름의 번역을 위해서 여기에 사용하지 않는 매크로로 키보드 이름을 
+ * 리스팅 한다.
+ * GTK+는 im_module_list() 함수로 지원하는 context 리스트를 얻은후 나중에 
+ * 메뉴에 보여 줄때에는 그 스트링에 해당하는 번역 스트링을 각 모듈에서 지정한
+ * mo 파일에서 로딩을 시도한다. 따라서 키보드 이름에 해당하는 번역 스트링을 
+ * imhangul의 mo 파일이 가지고 있어야 한다. 그러므로 imhangul이 libhangul의 
+ * 키보드 리스트를 동적으로 얻는다 하더라도, po 파일에 추가할 스트링은 
+ * 여기에 하드코딩할 수밖에 없다. 
+ * 아래에 리스팅한 키보드 이름은 번역되어 나타나겠지만, 없는 것들은 번역이 
+ * 안된상태로 메뉴에 나타나게 될 것이다. */
+#define CONTEXT_NAME_LIST 		    \
+    N_("Hangul Dubeolsik")		    \
+    N_("Hangul Dubeolsik yetgeul")	    \
+    N_("Hangul Sebeolsik dubeol layout")    \
+    N_("Hangul Sebeolsik 390")		    \
+    N_("Hangul Sebeolsik final")	    \
+    N_("Hangul Sebeolsik no-shift")	    \
+    N_("Hangul Sebeolsik yetgeul")	    \
+    N_("Hangul Romaja")			    \
+    N_("Hangul Ahnmatae")		    \
+}
+
 static GtkIMContext *im_hangul_new (const char *libhangul_id);
 
-static const GtkIMContextInfo hangul2_info = {
-  "hangul2",
-  N_("Hangul 2bul"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  DEFAULT_KEYBOARD_2 
-};
-
-static const GtkIMContextInfo hangul32_info = {
-  "hangul32",
-  N_("Hangul 3bul 2bul-shifted"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  ""
-};
-
-static const GtkIMContextInfo hangul39_info = {
-  "hangul39",
-  N_("Hangul 3bul 390"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  DEFAULT_KEYBOARD_39 
-};
-
-static const GtkIMContextInfo hangul3f_info = {
-  "hangul3f",
-  N_("Hangul 3bul Final"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  DEFAULT_KEYBOARD_3F 
-};
-
-static const GtkIMContextInfo hangul3s_info = {
-  "hangul3s",
-  N_("Hangul 3bul No-Shift"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  ""
-};
-
-static const GtkIMContextInfo hangul3y_info = {
-  "hangul3y",
-  N_("Hangul 3bul Yetgeul"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  ""
-};
-
-static const GtkIMContextInfo hangulro_info = {
-  "hangulro",
-  N_("Hangul Romaja"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  ""
-};
-
-static const GtkIMContextInfo hangulahn_info = {
-  "hangulahn",
-  N_("Hangul Ahnmatae"),
-  GETTEXT_PACKAGE,
-  IM_HANGUL_LOCALEDIR,
-  ""
-};
-
-static const GtkIMContextInfo *info_list[] = {
-  &hangul2_info,
-  &hangul32_info,
-  &hangul39_info,
-  &hangul3f_info,
-  &hangul3s_info,
-  &hangul3y_info,
-  &hangulro_info,
-  &hangulahn_info
-};
+/* 임의로 지원가능한 키보드 갯수를 16개로 잡는다. */
+static gchar context_ids[16][16];
+static gchar context_names[16][64];
+static GtkIMContextInfo info_array[16];
+static const GtkIMContextInfo *info_list[16];
 
 void
 im_module_init (GTypeModule *type_module)
@@ -124,8 +77,36 @@ void
 im_module_list (const GtkIMContextInfo ***contexts,
 		int *n_contexts)
 {
-  *contexts = info_list;
-  *n_contexts = G_N_ELEMENTS(info_list);
+    int i;
+    int n;
+
+    n = hangul_ic_get_n_keyboards();
+
+    /* 일단 현재로서는 info_array 개수를 벗어난 것은 리스팅 하지 않는다. */
+    if (n > G_N_ELEMENTS(info_array))
+	n = G_N_ELEMENTS(info_array);
+
+    for (i = 0; i < n; ++i) {
+	const char* id;
+	const char* name;
+	id = hangul_ic_get_keyboard_id(i);
+	name = hangul_ic_get_keyboard_name(i);
+	g_snprintf(context_ids[i], sizeof(context_ids[i]), "hangul%s", id);
+	g_snprintf(context_names[i], sizeof(context_names[i]),
+		    "Hangul %s", name);
+	info_array[i].context_id = context_ids[i];
+	info_array[i].context_name = context_names[i];
+	info_array[i].domain = GETTEXT_PACKAGE;
+	info_array[i].domain_dirname = IM_HANGUL_LOCALEDIR;
+	if (strcmp(id, DEFAULT_KEYBOARD_ID) == 0)
+	    info_array[i].default_locales = "ko";
+	else
+	    info_array[i].default_locales = "";
+	info_list[i] = &info_array[i];
+    }
+
+    *contexts = info_list;
+    *n_contexts = n;
 }
 
 GtkIMContext *
